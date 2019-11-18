c3f4a57292cb7cc8936edab417e4015da9213403
henry-jerry <jajawandera@gmail.com>
2019-11-18 10:23:19 +0200

    mac

--- src/DataImporter.py
+++ src/DataImporter.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
-data = pd.read_csv("../data/all_sites_v2.5.3.22.csv", sep=',', error_bad_lines=False)
+data_link = "../data/all_sites_v2.5.3.22.csv"
+data = pd.read_csv(data_link, sep=',', error_bad_lines=False)
 print(data)

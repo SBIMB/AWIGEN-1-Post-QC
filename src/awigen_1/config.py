945f7831e75f0ad5c5ddb7876ae6a432ba327011
henry_wandera <henry@WITS-USEs-MacBook-Pro.local>
2020-02-04 15:16:26 +0200

    my version

--- src/awigen_1/config.py
+++ src/awigen_1/config.py
@@ -8,6 +8,6 @@ import pandas as pd
 #this a global variable
 processed_data_dir = Path("../data/processed")
 
-data_link = processed_data_dir/'awigen_data.csv'
+data_link = processed_data_dir/'all_sites_v2.5.3.22.csv'
 
 awigen_1_data = pd.read_csv(data_link, sep=';', error_bad_lines=False, low_memory=False)
